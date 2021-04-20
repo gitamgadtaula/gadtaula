f0dd4704921277cab1dff15129667eeecdc04575
gitamgadtaula <gitamgadtaula@gmail.com>
2021-04-20 20:41:50 +0545

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Careers</li>
+      </ul>
+    </nav>
+    <div class="nuxt">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
+html {
+  font-family: "poppins", sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+nav {
+  position: fixed;
+  top: 0;
+  border: 1px solid 386fa4;
+  background-color: #071013;
+  padding: 10px 0;
+  width: 100%;
+  scroll-margin: 10px;
+}
+nav ul {
+  display: flex;
+  justify-content: flex-start;
+  gap: 20px;
+}
+nav li {
+  list-style-type: none;
+  color: white;
+  padding: 2px 4px;
+}
+nav li:hover {
+  /* background-color: aquamarine; */
+  cursor: pointer;
+  transform: scale(1.1);
+  animation: fade-in 0.5s ease-in-out;
+}
+@keyframes fade-in {
+  0% {
+    opacity: 0.3;
+  }
+  33% {
+    opacity: 0.4;
+  }
+  67% {
+    opacity: 0.6;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+.nuxt {
+  background-color: #f9fafb;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div class="main">
+      <div>
+        <h1>Gadtaula Group</h1>
+        <h3>We talk about startups that people like!</h3>
+      </div>
+    </div>
+    <div class="about-us">
+      <img src="@/assets/images/logo.png" />
+      <div class="about-title">
+        <h1>Who are we?</h1>
+        <h2>
+          We are enthusiastically driven group of people working rigorously for
+          the development of startup ideas that we believe will ease human lives
+        </h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.container {
+}
+.main {
+  background-image: url("@/assets/images/hero-bg.png");
+  height: 100vh;
+  width: 100%;
+  padding: 20px;
+  display: flex;
+  justify-content: flex-end;
+  background-attachment: fixed;
+  align-items: center;
+}
+h1,
+h2 {
+  /* color: #3d2348; */
+  font-weight: 1000;
+  padding: 4px 0px;
+}
+.about-us {
+  padding: 20px;
+  background-color: #f9fafb;
+  background-image: url("@/assets/images/ill.svg");
+  background-repeat: no-repeat;
+  /* background-size: cover; */
+  background-position: right;
+}
+
+.about-title {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+</style>
